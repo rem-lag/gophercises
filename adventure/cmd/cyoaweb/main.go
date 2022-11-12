b6fb9407ee2523b912ee208fd9fd204aaebc82fa
Remy Lagrois <rlagrois@oreilly.com>
2022-11-12 15:46:44 -0500

    serve first chapter of the story

--- adventure/cmd/cyoaweb/main.go
+++ adventure/cmd/cyoaweb/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"rem-lag/cyoa/teller"
 )
 
 func main() {
+	port := flag.Int("port", 8086, "port to start cyoa server on")
 	file := flag.String("file", "story.json", "JSON containing story and options")
 	flag.Parse()
 	fmt.Printf("Using story %s.\n", *file)
@@ -22,5 +25,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := teller.NewHandler(story)
+	fmt.Printf("Starting server on port %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- adventure/teller/story.go
+++ adventure/teller/story.go
@@ -2,20 +2,22 @@ package teller
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
 
 type Story map[string]Chapter
 
 type Chapter struct {
-	Title      string   `json:"title"`
-	Paragraphs []string `json:"story"`
-	Options    []Option `json:"options"`
+	Title      string    `json:"title"`
+	Paragraphs []string  `json:"story"`
+	Options    []Options `json:"options"`
 }
 
-type Option struct {
-	Text string `json:"text"`
-	Arc  string `json:"arc"`
+type Options struct {
+	Text    string `json:"text"`
+	Chapter string `json:"arc"`
 }
 
 func JsonStory(r io.Reader) (Story, error) {
@@ -29,3 +31,44 @@ func JsonStory(r io.Reader) (Story, error) {
 	return story, nil
 
 }
+
+type handler struct {
+	s Story
+}
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tmpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
+
+var tmpl *template.Template
+
+func init() {
+	tmpl = template.Must(template.New("").Parse(defaultHandlerTemp))
+}
+
+var defaultHandlerTemp = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Choose Your Own Adventure!!</title>
+    </head>
+    <body>
+        <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+            <p>{{.}}</p>
+        {{end}}
+        <ul>
+        {{range .Options}}
+            <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+        </ul>
+    </body>
+</html>`
