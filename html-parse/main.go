5f48e1b8a4e83d4bd92e5ad2ad61f63eb0ff01f5
Remy Lagrois <rlagrois@oreilly.com>
2022-11-15 20:28:49 -0500

    renames and add basic place holders

--- html-parse/main.go
+++ html-parse/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"rem-lag/htmlparse/hparse"
+	"strings"
+)
+
 func main() {
 
+	r := strings.NewReader(exHtml)
+	links, err := hparse.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
 }
+
+var exHtml = `
+<html>
+<body>
+  <h1>Hello!</h1>
+  <a href="/other-page">A link to another page</a>
+</body>
+</html>
+`
